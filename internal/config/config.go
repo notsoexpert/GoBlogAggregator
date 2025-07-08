499925a08547edd1a895a968025a71ba805b4018
notsoexpert <notsoexpert@gmail.com>
2025-07-08 16:55:30 -0500

    Implemented read, set user, and write functionality to config. Added state, command, and commands structs and implemented simple login command to main.

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,19 +1,56 @@
 package config
 
+import (
+	"encoding/json"
+	"os"
+)
+
 type Config struct {
 	DBUrl           string `json:"db_url"`
 	CurrentUserName string `json:"current_user_name"`
 }
 
-func Read() Config {
+func Read() (Config, error) {
+	path, err := getConfigFilePath()
+	if err != nil {
+		return Config{}, err
+	}
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return Config{}, err
+	}
+	var cfg Config
+	if err := json.Unmarshal(data, &cfg); err != nil {
+		return Config{}, err
+	}
+	return cfg, nil
+}
 
+func (cfg *Config) SetUser(userName string) error {
+	cfg.CurrentUserName = userName
+	if err := write(*cfg); err != nil {
+		return err
+	}
+	return nil
 }
 
 const configFileName = ".gatorconfig.json"
 
 func getConfigFilePath() (string, error) {
-	return "", nil
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	return homeDir + "/" + configFileName, nil
 }
 func write(cfg Config) error {
-	return nil
+	data, err := json.Marshal(cfg)
+	if err != nil {
+		return err
+	}
+	path, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(path, data, 0644)
 }
--- main.go
+++ main.go
@@ -1,5 +1,79 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+	"os"
 
+	"github.com/notsoexpert/goblogaggregator/internal/config"
+)
+
+type state struct {
+	Config *config.Config
+}
+
+type command struct {
+	Name string
+	Args []string
+}
+
+type commands struct {
+	Commands map[string]func(*state, command) error
+}
+
+func main() {
+	var currentState state
+	{
+		cfg, err := config.Read()
+		if err != nil {
+			fmt.Println("error reading config: ", err.Error())
+			os.Exit(1)
+		}
+		currentState.Config = &cfg
+	}
+
+	var commands commands
+	commands.Commands = make(map[string]func(*state, command) error)
+	commands.register("login", handlerLogin)
+
+	if len(os.Args) < 2 {
+		fmt.Println("error: not enough arguments")
+		os.Exit(1)
+	}
+
+	var loginCommand command = command{
+		Name: os.Args[1],
+		Args: os.Args[2:],
+	}
+
+	if err := commands.run(&currentState, loginCommand); err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	handler, ok := c.Commands[cmd.Name]
+	if !ok {
+		return errors.New("error: unregistered command")
+	}
+	return handler(s, cmd)
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.Commands[name] = f
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.Args) == 0 {
+		return errors.New("error: no username provided")
+	}
+
+	if err := s.Config.SetUser(cmd.Args[0]); err != nil {
+		return err
+	}
+
+	fmt.Println("User has been set to", cmd.Args[0])
+	return nil
 }
